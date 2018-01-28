e5d79a11e8fad40fa335c139baaa29a570d71766
Nafeez Abrar <nafeezabrar@gmail.com>
2018-01-28 15:40:32 +0600

    Async ReadByte and WriteByte added in Stream as Extension methods

---
+++ Xtra.Async/StreamExtension.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+
+namespace Xtra.Async
+{
+    public static class StreamExtension
+    {
+        public static async Task<int> ReadByteAsync(this Stream stream)
+        {
+            var buffer = new byte[1];
+            if (await stream.ReadAsync(buffer, 0, 1) == 0)
+                return -1;
+            return buffer[0];
+        }
+
+        public static Task WriteByteAsync(this Stream stream, byte value)
+        {
+            return stream.WriteAsync(new[] {value}, 0, 1);
+        }
+    }
+}
